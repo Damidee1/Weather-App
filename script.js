8d6ea92f3bbc352fe318591c144e5c5141ece802
KateStainton-Ellis <kate_se@yahoo.com>
2025-02-06 19:26:55 +0000

    html and css mods for background

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&
 
 // Call getWeather API when the button is clicked
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+// Need to add an EventListener to handle the "getWeather" click.
